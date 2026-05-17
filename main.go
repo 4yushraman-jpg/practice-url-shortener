c295ea0f599200d6f4f7c287f4c2792c4c428445
4yushraman-jpg <4yushraman@gmail.com>
2026-05-17 14:14:51 +0530

    main

--- main.go
+++ main.go
@@ -3,24 +3,50 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 	"url-shortener/database"
 	"url-shortener/handlers"
+	"url-shortener/middleware"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	dbPool, err := database.ConnectDB()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer dbPool.Close()
 
-	urlHandler := handlers.URLHandler{
-		DB: dbPool,
+	redisClient, err := database.ConnectRDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer redisClient.Close()
+
+	urlHandler := &handlers.URLHandler{
+		DB:  dbPool,
+		RDB: redisClient,
 	}
 
-	http.HandleFunc("/shorten", urlHandler.ShortenURLHandler)
+	rateLimiter := &middleware.RateLimitMiddleware{
+		RDB: redisClient,
+	}
 
-	http.HandleFunc("/", urlHandler.RedirectURLHandler)
+	// 1. Build the Expensive Limiter (20 requests / 1 min / "shorten" prefix)
+	expensiveMiddleware := rateLimiter.RateLimit(20, 1*time.Minute, "ratelimit:shorten")
+
+	// 2. Build the Cheap Limiter (1000 requests / 1 min / "redirect" prefix)
+	cheapMiddleware := rateLimiter.RateLimit(1000, 1*time.Minute, "ratelimit:redirect")
+
+	// Apply them to your routes!
+	http.HandleFunc("/shorten", expensiveMiddleware(urlHandler.ShortenURLHandler))
+	http.HandleFunc("/", cheapMiddleware(urlHandler.RedirectURLHandler))
 
 	port := ":8080"
 	log.Printf("Server starting on port %s...\n", port)
